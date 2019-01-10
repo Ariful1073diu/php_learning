a209010eaf1d8cfe4bc9369936512ee3927f5d4c
Md.Ariful Islam Anowar <ariful3c@gmail.com>
2019-01-11 00:56:02 +0600

    uUr lesrning start

---
+++ me .php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<?php
+	echo "this is my first php script";
+	?>
+	
+</body>
+</html>
